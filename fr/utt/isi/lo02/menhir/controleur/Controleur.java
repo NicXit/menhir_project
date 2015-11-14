82cedbfd527f7d487b3a46244596ae4cf1ae8c23
NicXit <nicolas.granett@utt.fr>
2015-11-14 15:29:45 +0100

    Controleur et Carte

---
+++ fr/utt/isi/lo02/menhir/controleur/Controleur.java
@@ -0,0 +1,10 @@
+package fr.utt.isi.lo02.menhir.controleur;
+
+public class Controleur {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ fr/utt/isi/lo02/menhir/modele/carte/CarteAllie.java
@@ -0,0 +1,9 @@
+package fr.utt.isi.lo02.menhir.modele.carte;
+
+public class CarteAllie {
+	private int[] value;
+	
+	public CarteAllie() {
+		
+	}
+}
