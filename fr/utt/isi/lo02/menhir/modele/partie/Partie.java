55a3c4b10c8db8515bfd40312f0059667da6f46b
NicXit <nicolas.granett@utt.fr>
2015-11-14 15:32:00 +0100

    Partie

--- fr/utt/isi/lo02/menhir/modele/partie/Partie.java
+++ fr/utt/isi/lo02/menhir/modele/partie/Partie.java
@@ -1,5 +1,16 @@
 package fr.utt.isi.lo02.menhir.modele.partie;
 
 public class Partie {
-
+	private TypePartie typePartie;
+	private Saison saison;
+	private Joueur[] ordreJeu;
+	private int nbJoueur, manche, tour;
+	
+	public Partie() {
+		
+	}
+	
+	
 }
+
+
