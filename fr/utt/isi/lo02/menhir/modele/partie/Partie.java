34b497a203ffe8922d97c9b096230c8d7eccb696
mat35770 <mat35770@hotmail.fr>
2015-11-17 16:46:46 +0100

    ajout mineurs

--- fr/utt/isi/lo02/menhir/modele/partie/Partie.java
+++ fr/utt/isi/lo02/menhir/modele/partie/Partie.java
@@ -1,5 +1,9 @@
 package fr.utt.isi.lo02.menhir.modele.partie;
 
+import fr.utt.isi.lo02.menhir.modele.enumeration.Saison;
+import fr.utt.isi.lo02.menhir.modele.enumeration.TypePartie;
+import fr.utt.isi.lo02.menhir.modele.joueur.Joueur;
+
 public class Partie {
 	private TypePartie typePartie;
 	private Saison saison;
