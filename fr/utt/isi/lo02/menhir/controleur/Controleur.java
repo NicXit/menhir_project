690baef85e7575efd24253c21920b2091dc805ec
NicXit <nicolas.granett@utt.fr>
2015-11-21 15:50:11 +0100

    controleur

--- fr/utt/isi/lo02/menhir/controleur/Controleur.java
+++ fr/utt/isi/lo02/menhir/controleur/Controleur.java
@@ -3,6 +3,7 @@ package fr.utt.isi.lo02.menhir.controleur;
 import fr.utt.isi.lo02.menhir.modele.carte.CarteAllie;
 import fr.utt.isi.lo02.menhir.modele.carte.CarteIngredient;
 import fr.utt.isi.lo02.menhir.modele.carte.Paquet;
+import java.util.Scanner;
 
 public class Controleur {
 
@@ -30,6 +31,27 @@ public class Controleur {
 	       		 System.out.println (v);
 		     }
 	       }**/
+		Scanner sc = new Scanner(System.in);
 		
+		System.out.println("*** JEU MENHIR ***");
+		
+		
+		System.out.println("Veuillez choisir un type de partie (rapide/avancée) ?");
+		String choix = sc.nextLine();
+		//choisirPartie(choix);
+		
+		System.out.println("Combien de joueurs Humains (2..6) ?");
+		int nbHumain = sc.nextInt();
+		
+		for(int i=0; i < nbHumain; i++){
+			//ajouterHumain();
+		}
+		
+		System.out.println("Combien de joueurs IA (2..5) ?");
+		int nbIA = sc.nextInt();
+		
+		for(int i=0; i < nbIA; i++){
+			//ajouterIA();
+		}
 	}		
 }
