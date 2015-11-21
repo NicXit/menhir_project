2ef9bad8af2fbd52bbfd7f6fea99643cb13923bd
NicXit <nicolas.granett@utt.fr>
2015-11-21 15:35:37 +0100

    initialisation cartes

--- fr/utt/isi/lo02/menhir/controleur/Controleur.java
+++ fr/utt/isi/lo02/menhir/controleur/Controleur.java
@@ -1,58 +1,35 @@
 package fr.utt.isi.lo02.menhir.controleur;
 
+import fr.utt.isi.lo02.menhir.modele.carte.CarteAllie;
 import fr.utt.isi.lo02.menhir.modele.carte.CarteIngredient;
+import fr.utt.isi.lo02.menhir.modele.carte.Paquet;
 
 public class Controleur {
 
 	public static void main(String[] args) {
-		CarteIngredient carteIngredient1 = new CarteIngredient({{1,2,3,4},{1,2,3,4},{1,2,3,4}});
-		int carte2[][] = {{1,2,3,4},{1,2,3,4},{1,2,3,4}};
-		int carte3[][] = {{1,2,3,4},{1,2,3,4},{1,2,3,4}};
-		int carte4[][] = {{1,2,3,4},{1,2,3,4},{1,2,3,4}};
-		int carte5[][] = {{1,2,3,4},{1,2,3,4},{1,2,3,4}};
-		int carte6[][] = {{1,2,3,4},{1,2,3,4},{1,2,3,4}};
-		int carte7[][] = {{1,2,3,4},{1,2,3,4},{1,2,3,4}};
-		int carte8[][] = {{1,2,3,4},{1,2,3,4},{1,2,3,4}};
-		int carte9[][] = {{1,2,3,4},{1,2,3,4},{1,2,3,4}};
-		int carte10[][] = {{1,2,3,4},{1,2,3,4},{1,2,3,4}};
-		int carte11[][] = {{1,2,3,4},{1,2,3,4},{1,2,3,4}};
-		int carte12[][] = {{1,2,3,4},{1,2,3,4},{1,2,3,4}};
-		int carte13[][] = {{1,2,3,4},{1,2,3,4},{1,2,3,4}};
-		int carte14[][] = {{1,2,3,4},{1,2,3,4},{1,2,3,4}};
-		int carte15[][] = {{1,2,3,4},{1,2,3,4},{1,2,3,4}};
-		int carte16[][] = {{1,2,3,4},{1,2,3,4},{1,2,3,4}};
-		int carte17[][] = {{1,2,3,4},{1,2,3,4},{1,2,3,4}};
-		int carte18[][] = {{1,2,3,4},{1,2,3,4},{1,2,3,4}};
-		int carte19[][] = {{1,2,3,4},{1,2,3,4},{1,2,3,4}};
-		int carte20[][] = {{1,2,3,4},{1,2,3,4},{1,2,3,4}};
-		int carte21[][] = {{1,2,3,4},{1,2,3,4},{1,2,3,4}};
-		int carte22[][] = {{1,2,3,4},{1,2,3,4},{1,2,3,4}};
-		int carte23[][] = {{1,2,3,4},{1,2,3,4},{1,2,3,4}};
-		int carte24[][] = {{1,2,3,4},{1,2,3,4},{1,2,3,4}};
-		CarteIngredient carte = new CarteIngredient(carte1);
-		CarteIngredient carte = new CarteIngredient(carte1);
-		CarteIngredient carte = new CarteIngredient(carte1);
-		CarteIngredient carte = new CarteIngredient(carte1);
-		CarteIngredient carte = new CarteIngredient(carte1);
-		CarteIngredient carte = new CarteIngredient(carte1);
-		CarteIngredient carte = new CarteIngredient(carte1);
-		CarteIngredient carte = new CarteIngredient(carte1);
-		CarteIngredient carte = new CarteIngredient(carte1);
-		CarteIngredient carte = new CarteIngredient(carte1);
-		CarteIngredient carte = new CarteIngredient(carte1);
-		CarteIngredient carte = new CarteIngredient(carte1);
-		CarteIngredient carte = new CarteIngredient(carte1);
-		CarteIngredient carte = new CarteIngredient(carte1);
-		CarteIngredient carte = new CarteIngredient(carte1);
-		CarteIngredient carte = new CarteIngredient(carte1);
-		CarteIngredient carte = new CarteIngredient(carte1);
-		CarteIngredient carte = new CarteIngredient(carte1);
-		CarteIngredient carte = new CarteIngredient(carte1);
-		CarteIngredient carte = new CarteIngredient(carte1);
-		CarteIngredient carte = new CarteIngredient(carte1);
-		CarteIngredient carte = new CarteIngredient(carte1);
-		CarteIngredient carte = new CarteIngredient(carte1);
-		CarteIngredient carte = new CarteIngredient(carte1);
-
-
+		/** TEST
+		Paquet paquet = new Paquet();
+		paquet.genererPaquetIngredient();
+		
+	       for(CarteIngredient c: paquet.cartesIngredient)
+	       {
+	       	 System.out.println (c.nom);
+	       	 for(int v: c.value)
+		     {
+	       		 System.out.println (v);
+		     }
+	       }
+	       
+	       paquet.genererPaquetAllie();
+			
+	       for(CarteAllie c: paquet.cartesAllie)
+	       {
+	       	 System.out.println (c.nom);
+	       	 for(int v: c.value)
+		     {
+	       		 System.out.println (v);
+		     }
+	       }**/
+		
+	}		
 }
--- fr/utt/isi/lo02/menhir/modele/carte/CarteAllie.java
+++ fr/utt/isi/lo02/menhir/modele/carte/CarteAllie.java
@@ -1,9 +1,10 @@
 package fr.utt.isi.lo02.menhir.modele.carte;
 
-public class CarteAllie {
-	private int[] value;
+public class CarteAllie extends Carte{
+	public int[] value;
 	
-	public CarteAllie() {
-		
+	public CarteAllie(String nom, int[] v) {
+		super(nom);
+		this.value = v;
 	}
 }
