4f11177a18385b83fc6a07a9413be5479d45ab1a
mat35770 <mat35770@hotmail.fr>
2015-12-19 17:36:09 +0100

    ajout des joueurs réels

---
+++ fr/utt/isi/lo02/menhir/vue/VueAjouterJoueurHumain.java
@@ -0,0 +1,53 @@
+package fr.utt.isi.lo02.menhir.vue;
+
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+import fr.utt.isi.lo02.menhir.controleur.Controleur;
+import fr.utt.isi.lo02.menhir.modele.partie.Partie;
+
+import java.text.NumberFormat;
+import java.util.Observable;
+import java.util.Observer;
+
+import javax.swing.JFormattedTextField;
+import javax.swing.JCheckBox;
+import javax.swing.JComboBox;
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.JButton;
+
+public class VueAjouterJoueurHumain extends JPanel {
+	
+	public VueAjouterJoueurHumain(Partie p, Controleur c) {		
+		
+		setLayout(null);		
+		JLabel lblNom = new JLabel("Nom :");
+		lblNom.setBounds(10, 11, 46, 14);
+		add(lblNom);
+		
+		JTextField textField = new JTextField();
+		textField.setBounds(55, 8, 86, 20);
+		add(textField);
+		textField.setColumns(10);
+		
+		JLabel lblAge = new JLabel("Age :");
+		lblAge.setBounds(183, 11, 46, 14);
+		add(lblAge);
+		
+		JFormattedTextField formattedTextField = new JFormattedTextField(NumberFormat.getIntegerInstance());
+		formattedTextField.setBounds(235, 8, 27, 20);
+		add(formattedTextField);
+		
+		JLabel lblSexe = new JLabel("Sexe :");
+		lblSexe.setBounds(285, 11, 46, 14);
+		add(lblSexe);
+		
+		JComboBox<String> comboBox = new JComboBox<String>();
+		comboBox.setModel(new DefaultComboBoxModel<String>(new String[] {"masculin", "f\u00E9minin"}));
+		comboBox.setToolTipText("");
+		comboBox.setBounds(344, 8, 79, 20);
+		add(comboBox);
+		
+	}
+}
--- fr/utt/isi/lo02/menhir/vue/VuePartie.java
+++ fr/utt/isi/lo02/menhir/vue/VuePartie.java
@@ -1,7 +1,10 @@
 package fr.utt.isi.lo02.menhir.vue;
 
 import java.awt.BorderLayout;
+import java.awt.CardLayout;
 import java.awt.EventQueue;
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.Observable;
 import java.util.Observer;
 
@@ -10,14 +13,29 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
 import fr.utt.isi.lo02.menhir.controleur.Controleur;
+import fr.utt.isi.lo02.menhir.modele.joueur.Joueur;
 import fr.utt.isi.lo02.menhir.modele.partie.Partie;
 import javax.swing.JTextField;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JComboBox;
+import javax.swing.JFormattedTextField;
+import javax.swing.BoxLayout;
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.JRadioButton;
+import javax.swing.JButton;
+import javax.swing.JSeparator;
+import java.awt.Color;
+import java.awt.event.ActionListener;
+import java.text.NumberFormat;
+import java.awt.event.ActionEvent;
 
 public class VuePartie extends JFrame implements Observer{
 	
 	private Partie partie;
 	private Controleur controleur;
-	private JPanel contentPane;
+	private JPanel container;
+	private JPanel vueAjouterJoueur;
 
 	/**
 	 * Create the frame.
@@ -28,15 +46,92 @@ public class VuePartie extends JFrame implements Observer{
 		partie.addObserver(this);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(contentPane);
-		contentPane.setLayout(null);
+		container = new JPanel();
+		container.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(container);
+		container.setLayout(null);
+		this.initContentPane();
+		
+	}
+	public void initContentPane(){
+		JLabel lblJeuDuMenhirs = new JLabel("Jeu du menhir");
+		lblJeuDuMenhirs.setFont(new Font("Calibri", Font.BOLD, 14));
+		lblJeuDuMenhirs.setBounds(163, 11, 97, 17);
+		container.add(lblJeuDuMenhirs);
+		
+		JComboBox<Integer> comboBox = new JComboBox<Integer>();
+		comboBox.setModel(new DefaultComboBoxModel<Integer>(new Integer[] {1, 2, 3, 4, 5, 6}));
+		comboBox.setBounds(214, 91, 46, 20);
+		container.add(comboBox);
+		
+		JLabel lblNombreDeJoueurs = new JLabel("Nombre de joueurs humains");
+		lblNombreDeJoueurs.setBounds(10, 94, 183, 14);
+		container.add(lblNombreDeJoueurs);
+		
+		JLabel lblNombreDeJoueurs_1 = new JLabel("Nombre de joueurs IA");
+		lblNombreDeJoueurs_1.setBounds(10, 119, 183, 14);
+		container.add(lblNombreDeJoueurs_1);
+		
+		JComboBox<Integer> comboBox_1 = new JComboBox<Integer>();
+		comboBox_1.setModel(new DefaultComboBoxModel<Integer>(new Integer[] {0,1,2,3,4,5}));
+		comboBox_1.setMaximumRowCount(5);
+		comboBox_1.setBounds(214, 116, 46, 20);
+		container.add(comboBox_1);
+		
+		JLabel lblTypeDePartie = new JLabel("Type de partie");
+		lblTypeDePartie.setBounds(10, 151, 136, 14);
+		container.add(lblTypeDePartie);
+		
+		JRadioButton rdbtnNewRadioButton = new JRadioButton("rapide");
+		rdbtnNewRadioButton.setBounds(215, 147, 79, 23);
+		container.add(rdbtnNewRadioButton);
+		
+		JRadioButton rdbtnNewRadioButton_1 = new JRadioButton("avanc\u00E9e\r\n");
+		rdbtnNewRadioButton_1.setBounds(305, 147, 109, 23);
+		container.add(rdbtnNewRadioButton_1);
+		
+		JButton btnOk = new JButton("OK");
+		btnOk.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				ajouterJoueur((int)comboBox.getSelectedItem());
+			}
+		});
+		btnOk.setBounds(171, 227, 89, 23);
+		container.add(btnOk);
+		
+		JLabel lblJoueursMaximum = new JLabel("2 \u00E0 6 joueurs");
+		lblJoueursMaximum.setBounds(10, 69, 183, 14);
+		container.add(lblJoueursMaximum);
+			
 		this.setVisible(true);
+	}
+	
+	public void ajouterJoueur(int nbJoueursHumain){
+		int i =0;
+		container.removeAll();
+		container.repaint();
+		
+		ArrayList<VueAjouterJoueurHumain> listeAjouterH = new ArrayList<VueAjouterJoueurHumain>();				
+		
+		for (i=0; i<nbJoueursHumain; i++){
+			listeAjouterH.add(new VueAjouterJoueurHumain(partie, controleur));			
+		}
+
+		i=0;
+		for (Iterator<VueAjouterJoueurHumain> it = listeAjouterH.iterator(); it.hasNext();){			
+			VueAjouterJoueurHumain vH = (VueAjouterJoueurHumain) it.next();
+			vH.setBounds(0, i, 450, 39);
+			container.add(vH);
+			i+=40;
+		}
+		
+		this.setVisible(true);
+
+		
+		
 	}
 	
 	public void update(Observable arg0, Object arg1){
 		
 	}
-
 }
