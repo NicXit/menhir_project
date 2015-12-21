99a67bae7dfcd7b9cf4c54656f92f64e30ebf364
mat35770 <mat35770@hotmail.fr>
2015-12-21 12:42:11 +0100

    ajout d'un controleur pour la vue

---
+++ fr/utt/isi/lo02/menhir/controleur/ControleurVue.java
@@ -0,0 +1,32 @@
+package fr.utt.isi.lo02.menhir.controleur;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Scanner;
+
+import fr.utt.isi.lo02.menhir.modele.carte.Paquet;
+import fr.utt.isi.lo02.menhir.modele.enumeration.Action;
+import fr.utt.isi.lo02.menhir.modele.enumeration.Saison;
+import fr.utt.isi.lo02.menhir.modele.partie.Partie;
+import fr.utt.isi.lo02.menhir.vue.VuePartie;
+
+public class ControleurVue {	
+	private Paquet paquet;	
+	private Partie p;
+	private VuePartie vp;
+	
+	int valeurJuste = 0, indiceChoix =0, nbHumain = 0, choixAction=1, choixCarte =0, numManche=0, value = 0, valCarte[], valueCarteAllie = 0;
+	String nomJoueurGagnant, typePartie;
+	char reponseBonusAvancee, choixTypePartie;
+	Action[] tabChoixAction = Action.values();
+	Saison[] tabSaison = Saison.values();
+	
+	public ControleurVue(){
+		paquet = new Paquet();
+		p = new Partie();
+		vp = new VuePartie(p, this);
+	}
+	
+	
+	
+}
